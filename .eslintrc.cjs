ad4bf56f95663a3af0dcbfc2af3c3d01ba08779d
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2023-03-02 21:18:10 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript','prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", '.ts', '.tsx'] }]
+  },
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      }
+    },
+    react: {
+      version: '^18.2.0'
+    }
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App (): JSX.Element {
+
+  return (
+    <div className="App">
+      
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default App
